d3a7e428d7564b1d13a03e717c70576553d3265c
Erick Oliveira de Souza <151755242+Erickd-Souza@users.noreply.github.com>
2024-03-29 00:39:12 -0300

    Ajuste Estrutural

---
+++ Jogo Xadrez/script.js
@@ -0,0 +1,11 @@
+function mover(){
+    let imagem = document.getElementById("aparecer");
+    if(imagem.style.display === 'none'){
+        document.getElementById('aparecer').style.display = 'block';
+        document.getElementById('sumir').style.display = 'none';
+        
+    }else{
+        document.getElementById('aparecer').style.display = 'none';
+        document.getElementById('sumir').style.display = 'block';
+    }
+}
