aa733bd59eea79e07ed447dd3b94a88d91de8e50
Erick Oliveira de Souza <151755242+Erickd-Souza@users.noreply.github.com>
2024-03-29 00:25:49 -0300

    Update script.js

--- Jogo Xadrez/site/Javascript/script.js
+++ Jogo Xadrez/site/Javascript/script.js
@@ -2,8 +2,7 @@ function mover(){
     let imagem = document.getElementById("aparecer");
     if(imagem.style.display === 'none'){
         document.getElementById('aparecer').style.display = 'block';
-        document.getElementById('sumir').style.display = 'none';
-        
+        document.getElementById('sumir').style.display = 'none';  
     }else{
         document.getElementById('aparecer').style.display = 'none';
         document.getElementById('sumir').style.display = 'block';
